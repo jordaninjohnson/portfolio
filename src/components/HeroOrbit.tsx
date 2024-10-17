d93f867e32d5a0bcc6551f6bd173b1ad0f2b47bd
Jordan Johnson <jordaninjohnson@gmail.com>
2024-10-17 16:07:38 -0600

    Time 1:19:31

--- src/components/HeroOrbit.tsx
+++ src/components/HeroOrbit.tsx
@@ -8,14 +8,21 @@ export const HeroOrbit = ({
   return (
     <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
       <div
-        className="border border-red-500"
+        className="flex items-start justify-start"
         style={{
           transform: `rotate(${rotation}deg)`,
           height: `${size}px`,
           width: `${size}px`,
         }}
       >
-        <div className="border border-red-500 inline-flex ">{children}</div>
+        <div
+          className="inline-flex"
+          style={{
+            transform: `rotate(${rotation * -1}`,
+          }}
+        >
+          {children}
+        </div>
       </div>
     </div>
   );
